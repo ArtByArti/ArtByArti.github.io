1714afd3afebd679f2d53e1aadaf81d8b8973312
ArtByArti <sarsavbuk@gmail.com>
2023-09-25 18:15:39 +0700

    Create Message Icons

---
+++ js/message.js
@@ -0,0 +1,5 @@
+document.querySelector(".menu-toggle").addEventListener("click", function() {
+    document.querySelector(".menu-toggle").classList.toggle("open");
+    document.querySelector(".menu-line").classList.toggle("open");
+  });
+  
